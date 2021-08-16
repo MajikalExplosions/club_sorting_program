bb3f43b38610381c2e30550702e0ebc76f724a41
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2021-08-16 11:51:17 -0700

    Add CSV reader

---
+++ ClubSortingProgram2/Solver/Response.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ClubSortingProgram2.Solver
+{
+    public class Response
+    {
+        public readonly string FileName;
+        private string[][] _data;
+        private readonly bool _success;
+
+        public Response(string fileName)
+        {
+            //It's not successful if it returns early
+            _success = false;
+
+            //Check for file existence
+            if (!File.Exists(fileName))
+            {
+                MainScreen.Instance.AlertError("File " + fileName + " doesn't exist.");
+                return;
+            }
+
+            //Check for file extension
+            string type = fileName.Substring(fileName.LastIndexOf('.') + 1).ToUpper();
+            if (fileName.LastIndexOf('.') == -1)
+            {
+                MainScreen.Instance.AlertError("Missing filename extension.");
+                return;
+            }
+
+            //Read contents
+            string contents = File.ReadAllText(fileName, Encoding.UTF8);
+            List<List<string>> parsedLines = null;
+
+            //Parse to lines; returns null if file couldn't be parsed
+            if (type == "CSV")
+            {
+                parsedLines = _parseToLines(contents, ',');
+            }
+            else if (type == "TSV")
+            {
+                parsedLines = _parseToLines(contents, '\t');
+            }
+            //TODO Maybe add support for other file types?
+            else
+            {
+                MainScreen.Instance.AlertError("Unsupported file type.");
+            }
+
+            //Make sure it was parsed successfully; if it wasn't, errors would have been thrown.
+            if (parsedLines == null) return;
+
+            //Check to make sure there's at least one line so toArray doesn't break
+            parsedLines = _padLength(_removeEmpty(parsedLines));
+            if (parsedLines.Count == 0)
+            {
+                MainScreen.Instance.AlertError("Empty file.");
+                return;
+            }
+            _data = toArray(parsedLines);
+            _success = true;
+        }
+
+        public bool Successful()
+        {
+            return _success;
+        }
+
+        public string[] GetHeaders()
+        {
+            if (!_success)
+            {
+                MainScreen.Instance.AlertError("Could not get headers from bad file.");
+            }
+
+            //Make a shallow copy
+            return (string[])_data[0].Clone();
+        }
+
+        private List<List<string>> _parseToLines(string text, char delimiter)
+        {
+            //Make sure the text ends in a newline
+            text += "\n";
+            List<List<string>> final = new List<List<string>>();
+            final.Add(new List<string>());
+
+            //Keep track of state
+            bool inQuotes = false;
+            bool escaped = false;
+            string currentString = "";
+
+            foreach (char c in text)
+            {
+                if (escaped)
+                {
+                    //Escaped characters always go through (like \")
+                    currentString += c;
+                    escaped = false;
+                }
+                else if (c == delimiter)
+                {
+                    //If we find the delimiter, create a new line unless it's in quotes
+                    if (inQuotes) currentString += c;
+                    else
+                    {
+                        //Add the current word to the line
+                        final[final.Count - 1].Add(currentString.Trim());
+                        currentString = "";
+                    }
+                }
+                else if (c == '\\')
+                {
+                    escaped = true;
+                }
+                else if (c == '\n')
+                {
+                    if (inQuotes) currentString += c;
+                    else
+                    {
+                        //New line, so add the last word and create a new line
+                        final[final.Count - 1].Add(currentString.Trim());
+                        currentString = "";
+                        final.Add(new List<string>());
+                    }
+                }
+                else if (c == '\"')
+                {
+                    //Can't be escaped, so is a normal one
+                    inQuotes = !inQuotes;
+                }
+                else
+                {
+                    //All other characters
+                    currentString += c;
+                }
+            }
+            if (escaped || inQuotes || currentString.Trim().Length != 0)
+            {
+                MainScreen.Instance.AlertError("Could not parse file (incomplete).");
+                return null;
+            }
+            return final;
+        }
+
+        private List<List<string>> _removeEmpty(List<List<string>> arr)
+        {
+            List<List<string>> final = new List<List<string>>();
+
+            foreach (List<string> l in arr)
+            {
+                if (l.Count != 0) final.Add(l);
+            }
+
+            return final;
+        }
+
+        private List<List<string>> _padLength(List<List<string>> arr)
+        {
+            int max = -1;
+            foreach (List<string> l in arr)
+            {
+                max = Math.Max(l.Count, max);
+            }
+
+            foreach (List<string> l in arr)
+            {
+                while (l.Count < max) l.Add("");
+            }
+
+            return arr;
+        }
+
+        private string[][] toArray(List<List<string>> lst)
+        {
+            int cols = lst.Count;
+            int rows = lst[0].Count;
+            string[][] arr = new string[cols][];
+            for (int i = 0; i < cols; i++)
+            {
+                if (lst[i].Count != rows)
+                {
+                    MainScreen.Instance.AlertError("Could not convert List<List<string>> to string[,].");
+                    return null;
+                }
+                arr[i] = new string[rows];
+                for (int j = 0; j < rows; j++)
+                {
+                    arr[i][j] = lst[i][j];
+                }
+            }
+
+            return arr;
+        }
+    }
+}
