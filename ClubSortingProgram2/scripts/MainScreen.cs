b38f847bc115a8e3bad0c86930c81e3f7de60283
MajikalExplosions <31389551+MajikalExplosions@users.noreply.github.com>
2021-08-29 15:13:29 -0700

    Make buttons work

--- ClubSortingProgram2/scripts/MainScreen.cs
+++ ClubSortingProgram2/scripts/MainScreen.cs
@@ -1,20 +1,57 @@
+using ClubSortingProgram2.Solver;
 using Godot;
 
-public class MainScreen : Container
+public class MainScreen : Control
 {
     public static MainScreen Instance;
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    private FileDialog _PopupSelector;
+    private AcceptDialog _PopupError;
+    private WindowDialog _PopupHelp;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        _PopupSelector = (FileDialog)GetNode("Popup_Selector");
+        _PopupSelector.PopupExclusive = true;
+
+        _PopupError = (AcceptDialog)GetNode("Popup_Error");
+        _PopupError.PopupExclusive = true;
+
+        _PopupHelp = (WindowDialog)GetNode("Popup_Help");
+        _PopupHelp.PopupExclusive = true;
     }
 
-    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-    //  public override void _Process(float delta)
-    //  {
-    //
-    //  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame. public override
+    private void _Process(float delta)
+    {
+    }
+
+    public void OnHelpPressed()
+    {
+        _PopupHelp.PopupCentered();
+    }
+
+    public void OnFileSelectPressed()
+    {
+        _PopupSelector.PopupCentered();
+    }
+
+    public void OnFileSelectConfirmed()
+    {
+        string file = _PopupSelector.CurrentPath;
+        try
+        {
+            Response response = new Response(file);
+        }
+        catch (SolverException e)
+        {
+            _ShowException(e);
+        }
+    }
+
+    private void _ShowException(SolverException e)
+    {
+        _PopupError.DialogText = e.Message;
+        _PopupError.PopupCentered();
+    }
 }
